a035672f6ade6174fce0f54dc288bbc2f3b65abe
christina-sol <christina.halder@me-solshare.com>
2020-11-17 00:16:46 +0600

    Fixed Index page design and content

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,10 +1,18 @@
 class RecipeController < ApplicationController
 
-  # GET /contents
+  # GET /recipe
   def index
+    #  This method renders all the contentful entries of type 'recipe' and show as list.
   end
 
-  # GET /contents/1
+  # GET /recipe/1
   def show
+    # This method takes the recipe id as input, calls the contentful api to get details
+    # of the specific recipe and show the details.
+    # params : {
+    #     "controller"=>"recipe",
+    #     "action"=>"show",
+    #     "id"=>"4dT8tcb6ukGSIg2YyuGEOm"
+    # }
   end
 end
