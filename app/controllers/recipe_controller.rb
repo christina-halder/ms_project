991dfc2d3f9e6e66666f630a98298c251c1a4359
christina-sol <christina.halder@me-solshare.com>
2020-12-03 00:52:50 +0600

    normalized visualizing data on show method

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -5,8 +5,6 @@ class RecipeController < ApplicationController
   # GET /recipe
   def index
     #  This method renders all the contentful entries of type 'recipe' and show as list.
-    # @recipes = ContentfulClient.new.entries('recipe',1)
-    # @recipes = Recipe.all.params({"include" => 1}).load
     @recipes = Recipe.all.load
   end
 
@@ -15,12 +13,11 @@ class RecipeController < ApplicationController
     # This method takes the recipe id as input, calls the ContentfulClient class's entry method
     # which calls the contentful api to get details of the specific recipe and show the details.
     # params : {
-    #     "controller"=>"recipe",
-    #     "action"=>"show",
     #     "id"=>"4dT8tcb6ukGSIg2YyuGEOm"
     # }
 
     # @recipe = ContentfulClient.new.entry(params[:id])
-    @recipe = Recipe.find(params[:id])
+    recipe_details = Recipe.find(params[:id])
+    @details = recipe_details.normalize
   end
 end
--- config/initializers/contentful.rb
+++ config/initializers/contentful.rb
@@ -27,5 +27,6 @@ ContentfulRails.configure do |config|
   config.access_token = "7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c"
   config.space = "kk2bw5ojx476"
   config.environment = "master"
+  config.perform_caching = true
   # config.contentful_options = "hash of options"
 end
