294c21ee491e9cc5205869a4509ee3f1579e3ab6
christina-halder <chalder901@gmail.com>
2020-12-05 01:56:56 +0600

    Added pagination with will_paginate gem

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -5,7 +5,13 @@ class RecipeController < ApplicationController
   # GET /recipe
   def index
     #  This method renders all the contentful entries of type 'recipe' and show as list.
-    @recipes = Recipe.all.load
+
+    page_count = (params[:page].to_i > 0) ? params[:page].to_i : 1
+    per_page = 2
+    @recipes = Recipe.paginate(page = page_count, per_page = per_page, order_field = 'sys.updatedAt').load
+    @recipes_paginate = WillPaginate::Collection.create(page_count, per_page, @recipes.total) do |p|
+      p.replace(@recipes.to_a)
+    end
   end
 
   # GET /recipe/1
