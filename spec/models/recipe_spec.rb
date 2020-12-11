ec4837976305f3b3b6267d676ae79c7fa94c0f20
christina-halder <chalder901@gmail.com>
2020-12-12 00:02:38 +0600

    Specs and VCR Setup

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Recipe do
+
   it 'content_type_id is eq recipe' do
     expect(Recipe.content_type_id).to eq 'recipe'
   end
 
-  it 'content_type_id is eq recipe' do
-    expect(Recipe.coerce_field).to eq expected_coerce_fields
+  describe 'coerce fields' do
+    let(:expected_coerce_fields) do
+      {:title=>:String, :description=>:String, :chef=>:String, :tags=>:Array}
+    end
+
+    it 'content_type_id is eq recipe' do
+      expect(Recipe.coerce_field).to eq expected_coerce_fields
+    end
   end
 end
